3ab6fea36cebd80d63ccdee0bcd688776be08b57
cheese_ge <cheese5skin@gmail.com>
2024-07-21 00:52:41 +0800

    navbar css

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,13 @@
 //app是所有頁面共用的最上層文件
 //任何在 _app.js 中匯入的樣式都會套用到整個應用程式的所有頁面。
 import '../styles/global.css'
+import Navbar from '../components/navbar';
 
 export default function App({ Component, pageProps}) {
-    return <Component {...pageProps} />;
+    return (
+        <div>
+            <Navbar />
+            <Component {...pageProps} />
+        </div>
+    )
 }
