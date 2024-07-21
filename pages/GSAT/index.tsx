e50f65a1ed19edca64747b6d21bd75a837f46397
charhhhhhh <charliewang1201@gmail.com>
2024-07-21 22:28:46 +0800

    update GSAT

--- pages/GSAT/index.tsx
+++ pages/GSAT/index.tsx
@@ -1,18 +1,45 @@
 import React from "react";
-import Item from "./item";
+import { Score, School, Chooseyear, Light } from "./item";
+import style from "../../styles/GSATitem.module.css";
 const GSAT = () => {
   return (
-    <div>
-      <h1>請輸入成績 </h1>
-      <form action="">
-        <Item name="國文" subject="chinese" />
-        <Item name="英文" subject="english" />
-        <Item name="數A " subject="mathA" />
-        <Item name="數B " subject="mathB" />
-        <Item name="自然" subject="nature" />
-        <Item name="社會" subject="society" />
-      </form>
-    </div>
+    <>
+      <div>
+        <h1 className={style.title}>請輸入成績 </h1>
+        <form action="">
+          <Score name="國文" subject="chinese" className={style.obj} />
+          <Score name="英文" subject="english" className={style.obj} />
+          <Score name="數A " subject="mathA" className={style.obj} />
+          <Score name="數B " subject="mathB" className={style.obj} />
+          <Score name="自然" subject="nature" className={style.obj} />
+          <Score name="社會" subject="society" className={style.obj} />
+        </form>
+      </div>
+
+      <h1 className={style.title}>選擇要查詢科系</h1>
+      <label htmlFor="school">選擇學校：</label>
+      <School className={style.Sele} />
+      <br></br>
+      <label htmlFor="major">選擇系所：</label>
+      <School className={style.Sele} />
+
+      <h1 className={style.title}>參考年度（複選）</h1>
+      <Chooseyear max_year="113" className={style.Chooseyear} />
+      <br></br>
+
+      <div>
+        <h1 className={style.title}>自訂危險程度</h1>
+        <form action="">
+          <Light name="紅燈" color="red" className={style.red_light} />
+          <Light name="黃燈" color="yellow" className={style.yellow_light} />
+          {/* or 綠燈<=黃燈標準<=紅燈 */}
+          <Light name="綠燈" color="green" className={style.green_light} />
+        </form>
+      </div>
+
+      {/* <p className={style.redlight}>紅燈</p> */}
+      <button className={style.submitbutton}>提交！</button>
+    </>
   );
 };
 
--- pages/GSAT/item.tsx
+++ pages/GSAT/item.tsx
@@ -1,6 +1,6 @@
-const Item = ({ name, subject }) => {
+const Score = ({ name, subject, className }) => {
   return (
-    <div>
+    <div className={className}>
       <label htmlFor="">{name}級分：</label>
       <input
         type="number"
@@ -8,10 +8,52 @@ const Item = ({ name, subject }) => {
         id={subject}
         min="0"
         max="15"
-        defaultValue={0}
+        defaultValue={""}
         className="hide-arrows"
       />
     </div>
   );
 };
-export default Item;
+
+const School = ({ className }) => {
+  return (
+    //選項先放著
+    <select name="cars" id="cars" className={className}>
+      <option value="volvo">Volvo</option>
+      <option value="saab">Saab</option>
+      <option value="mercedes">Mercedes</option>
+      <option value="audi">Audi</option>
+    </select>
+  );
+};
+
+const Chooseyear = ({ max_year, className }) => {
+  const years = [];
+  for (let i = parseInt(max_year), cnt = 0; i >= 111; cnt++, i--) {
+    years.push(<label htmlFor="">{i}</label>);
+    years.push(
+      <input type="checkbox" name={i.toString()} className={className} />
+    );
+    if ((cnt + 1) % 5 == 0) years.push(<br></br>);
+  }
+  return years;
+};
+
+const Light = ({ name, color, className }) => {
+  return (
+    <div className={className}>
+      <label htmlFor="" id={color}>
+        {name}：
+      </label>
+      <input
+        type="number"
+        name={color}
+        min="1"
+        defaultValue={""}
+        className="hide-arrows"
+      />
+    </div>
+  );
+};
+
+export { Score, School, Chooseyear, Light };
