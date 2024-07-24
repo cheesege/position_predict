9b996d4d22075f8141f7fbeb7d624fff6aa19b46
charhhhhhh <charliewang1201@gmail.com>
2024-07-24 16:12:04 +0800

    update submit

--- pages/GSAT/submit.js
+++ pages/GSAT/submit.js
@@ -8,6 +8,9 @@ const submit = () =>{
         else if(Number(document.getElementById(subject[i]).value) > 15){
             alert("輸入之級分超過15\n請重新輸入")
             return
+        }else if(Number(document.getElementById(subject[i]).value) <= -1){
+            alert("輸入之級分必須大於等於0\n請重新輸入")
+            return
         }
         else{
             score.push(Number(document.getElementById(subject[i]).value));
@@ -31,6 +34,10 @@ const submit = () =>{
             alert("輸入之APCS級分超過5\n請重新輸入")
             return
         }
+        if(apcs[i]<=-1){
+            alert("輸入之APCS級分必須大於等於0\n請重新輸入")
+            return
+        }
     }
     console.log(apcs)
 
@@ -41,7 +48,7 @@ const submit = () =>{
         }
     }
     if(year.length == 0){
-        alert("您未選取參考年度\n請重新輸入")
+        alert("未選取參考年度\n請重新輸入")
         return
     }
     console.log(year)
@@ -51,6 +58,18 @@ const submit = () =>{
     dan.push(document.getElementById("yellow").value)
     dan.push(document.getElementById("green").value)
     console.log(dan)
+
+    var school = []
+    let cnt=document.getElementById("choose_school")
+    for(const i of cnt.children ){
+        school.push(i.textContent);
+        console.log(i.textContent);
+    }
+    if(cnt.children.length == 0){
+        alert("未選取任一校系\n請重新輸入")
+        return
+    }
+
 }
 
 export {submit}
